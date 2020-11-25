0f8ebd0dfd3616c66229b680058f0c4d418ab5aa
Sialsanwal <56753162+Sialsanwal@users.noreply.github.com>
2020-11-25 08:12:16 +0500

    added bootomNavbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import NavBar from './Component/navBar';
 import InfoPanel from './Component/infoPanel';
+import BootomNav from './Component/bootomNav';
 function App() {
   return (
     <div >
     <NavBar/>
     <InfoPanel/>
+    <br/>
+    <BootomNav/>
     </div>
   );
 }
